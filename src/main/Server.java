0be1865a64c8ed4da06f7c7032779fa2db851eec
LewysW <lewysward99@gmail.com>
2018-07-14 22:40:36 +0100

    Added basic half duplex communication between the client and server where the server retransmits the clients message.

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,8 +1,6 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -12,17 +10,20 @@ public class Server {
     //Default values provided if no arguments are provided during execution.
     private static final String DEFAULT_IP = "127.0.0.1";
     private static final String DEFAULT_PORT = "1362";
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(9090);
         try {
             while (true) {
                 Socket socket = listener.accept();
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
                 try {
-                    Scanner scanner = new Scanner(System.in);
-                    PrintWriter out =
-                            new PrintWriter(socket.getOutputStream(), true);
-                    out.println(scanner.nextLine());
+                    while (true) {
+                        dataOutputStream.writeUTF(dataInputStream.readUTF());
+                    }
                 } finally {
                     socket.close();
                 }
