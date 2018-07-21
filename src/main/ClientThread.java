b2957e0b49d7963fffa14ed930115578f8889eee
LewysW <lewysward99@gmail.com>
2018-07-21 17:11:03 +0100

    Made the program multithreaded so that multiple user's could connect and create chats

---
+++ src/main/ClientThread.java
@@ -0,0 +1,100 @@
+package main;
+
+import model.Chat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class ClientThread extends Thread {
+    private static final int JOIN = 0;
+    private static final int CREATE = 1;
+    private static final int OK = 0;
+    private static final int ERROR = 1;
+    private Socket socket;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            System.out.println("Hello, World!");
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                //Handles initial client request to join or create chat:
+                int clientRequest = dataInputStream.readByte();
+                handleRequest(clientRequest, dataInputStream, dataOutputStream, Server.getChats());
+
+                printChats(Server.getChats());
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static HashMap<String, String> addChat(HashMap<String, String> passwords, String chatName, String hashedPass) {
+        passwords.put(chatName, hashedPass);
+        return passwords;
+    }
+
+
+
+    public static boolean authenticate(String chatName, String hashedPass, HashMap<String, String> passwords) {
+        return (hashedPass.equals(passwords.get(chatName)));
+    }
+
+    public static void handleRequest(int request, DataInputStream dataInputStream, DataOutputStream dataOutputStream, HashMap<String, Chat> chats) throws IOException {
+        if (request == CREATE) {
+            String chatName = dataInputStream.readUTF();
+            String hashedPass = dataInputStream.readUTF();
+            Boolean passEnabled = dataInputStream.readBoolean();
+            Boolean logEnabled = dataInputStream.readBoolean();
+
+            //TODO add regex for password to ensure security
+            if (validChat(passEnabled, hashedPass, chatName) && !chatExists(chatName, chats)) {
+                hashedPass = passEnabled ? hashedPass : null;
+                Chat chat = new Chat(chatName, hashedPass, logEnabled);
+                chats.put(chatName, chat);
+                dataOutputStream.writeByte(OK);
+            } else {
+                dataOutputStream.writeByte(ERROR);
+            }
+
+        } else if (request == JOIN) {
+            //JOIN code goes here
+        } else {
+            System.out.println("Invalid request");
+        }
+    }
+
+    public static boolean chatExists(String name, HashMap<String, Chat> chats) {
+        return chats.containsKey(name);
+    }
+
+    public static boolean validChat(Boolean passEnabled, String hashedPass, String chatName) {
+        //True if chatName is not empty, and password is enabled and valid, or if password is disabled
+        return (!chatName.equals("")) && ((passEnabled && !hashedPass.equals("")) || (!passEnabled));
+    }
+
+    public static void printChats(HashMap<String, Chat> chats) {
+        Iterator it = chats.entrySet().iterator();
+
+        while (it.hasNext()) {
+            HashMap.Entry pair = (HashMap.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+        }
+    }
+
+}
--- src/main/Server.java
+++ src/main/Server.java
@@ -13,91 +13,28 @@ public class Server {
     //Default values provided if no arguments are provided during execution.
     private static final String DEFAULT_IP = "127.0.0.1";
     private static final int DEFAULT_PORT = 10000;
-    private static DataInputStream dataInputStream;
-    private static DataOutputStream dataOutputStream;
-    private static final int JOIN = 0;
-    private static final int CREATE = 1;
-    private static final int OK = 0;
-    private static final int ERROR = 1;
+
     //TODO load in chats
     private static HashMap<String, Chat> chats = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(10000);
 
-        while (true) {
+        while (!listener.isClosed()) {
             try {
                 System.out.println("Hi");
-                Socket socket = listener.accept();
-                dataInputStream = new DataInputStream(socket.getInputStream());
-                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                while (true) {
-                    //Handles initial client request to join or create chat:
-                    int clientRequest = dataInputStream.readByte();
-                    handleRequest(clientRequest, dataInputStream, dataOutputStream);
-
-                    printChats();
-
-
-                }
-            } finally {
-                listener.close();
+                new Thread(new ClientThread(listener.accept())).start();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-
-    public static HashMap<String, String> addChat(HashMap<String, String> passwords,String chatName, String hashedPass) {
-        passwords.put(chatName, hashedPass);
-        return passwords;
+    public static HashMap<String, Chat> getChats() {
+        return chats;
     }
 
-
-
-    public static boolean authenticate(String chatName, String hashedPass, HashMap<String, String> passwords) {
-        return (hashedPass.equals(passwords.get(chatName)));
-    }
-
-    public static void handleRequest(int request, DataInputStream dataInputStream, DataOutputStream dataOutputStream) throws IOException {
-        if (request == CREATE) {
-            String chatName = dataInputStream.readUTF();
-            String hashedPass = dataInputStream.readUTF();
-            Boolean passEnabled = dataInputStream.readBoolean();
-            Boolean logEnabled = dataInputStream.readBoolean();
-
-            //TODO add regex for password to ensure security
-            if (validChat(passEnabled, hashedPass, chatName) && !chatExists(chatName)) {
-                hashedPass = passEnabled ? hashedPass : null;
-                Chat chat = new Chat(chatName, hashedPass, logEnabled);
-                chats.put(chatName, chat);
-                dataOutputStream.writeByte(OK);
-            } else {
-                dataOutputStream.writeByte(ERROR);
-            }
-
-        } else if (request == JOIN) {
-            //JOIN code goes here
-        } else {
-            System.out.println("Invalid request");
-        }
-    }
-
-    public static boolean chatExists(String name) {
-        return chats.containsKey(name);
-    }
-
-    public static boolean validChat(Boolean passEnabled, String hashedPass, String chatName) {
-        //True if chatName is not empty, and password is enabled and valid, or if password is disabled
-        return (!chatName.equals("")) && ((passEnabled && !hashedPass.equals("")) || (!passEnabled));
-    }
-
-    public static void printChats() {
-        Iterator it = chats.entrySet().iterator();
-
-        while (it.hasNext()) {
-            HashMap.Entry pair = (HashMap.Entry)it.next();
-            System.out.println(pair.getKey() + " = " + pair.getValue());
-        }
+    public static void setChats(HashMap<String, Chat> chats) {
+        Server.chats = chats;
     }
 }
