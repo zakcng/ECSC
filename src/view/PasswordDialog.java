1b54a4470985c6f8b4f14cdc9b1ad0165100cf7f
Zak <zakcng@gmail.com>
2018-08-27 11:17:30 +0100

    Updated dialog to include label

--- src/view/PasswordDialog.java
+++ src/view/PasswordDialog.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -14,7 +15,7 @@ public class PasswordDialog extends Dialog<String> {
 
     public PasswordDialog() {
         setTitle("Password");
-        setHeaderText("Please enter your password.");
+        setHeaderText("Please enter chat password.");
 
         ButtonType passwordButtonType = new ButtonType("Ok", ButtonData.OK_DONE);
         getDialogPane().getButtonTypes().addAll(passwordButtonType, ButtonType.CANCEL);
@@ -22,8 +23,10 @@ public class PasswordDialog extends Dialog<String> {
         passwordField = new PasswordField();
         passwordField.setPromptText("Password");
 
+        Label lblText = new Label("Please enter the chat password: ");
+
         HBox hBox = new HBox();
-        hBox.getChildren().add(passwordField);
+        hBox.getChildren().addAll(lblText, passwordField);
         hBox.setPadding(new Insets(20));
 
         HBox.setHgrow(passwordField, Priority.ALWAYS);
